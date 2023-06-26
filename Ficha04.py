b24f9b0255ea3b46dbb48032a58178e3b0ea8962
ricardo-henriques-silva <rhcsilva@gmail.com>
2023-06-26 11:09:56 +0100

    Explorar funções do Numpy

--- Ficha04.py
+++ Ficha04.py
@@ -6,7 +6,7 @@ dados = pd.read_excel('P_Data_Extract_From_World_Development_Indicators.xlsx', s
 # Selecionar as colunas de interesse (País, Ano e Consumo de Energia)
 dados = dados[['Country Name', '1990 [YR1990]', '2000 [YR2000]']]
 
-# Filtrar os dados para o Brasil
+# Filtrar os dados para Portugal
 dados_portugal = dados[dados['Country Name'] == 'Portugal']
 
 # Calcular a variação percentual
@@ -20,3 +20,20 @@ consumo_2000 = dados_portugal['2000 [YR2000]'].values[0]
 # Calcular e imprimir a variação percentual
 variacao_percentual = calcular_variacao_percentual(consumo_1990, consumo_2000)
 print(f'Variação percentual no consumo de energia elétrica em Portugal entre 1990 e 2000: {variacao_percentual:.2f}%')
+
+
+# # === Explorar funções do Numpy
+
+import numpy as np
+anos = np.array([2001,2011,2021])
+PIB_milhoes = np.array([ 139222.3, 180748.3,187432.5])
+Investimento_percent = np.array([0.267,0.274,0.184]) 
+
+Investimento_milhoes = PIB_milhoes*Investimento_percent
+
+print('\nInvestimento em milhões de euros nos 3 censos do séc XXI')
+print(anos)
+print(Investimento_milhoes)
+
+print('\nMédia do Investimento em milhões de euros')
+print(np.mean(Investimento_milhoes))
